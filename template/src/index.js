ef7e5cb873c399138542a73ab2b22633359292c9
Ken Wheeler <thekenwheeler@gmail.com>
2017-08-30 10:09:23 -0400

    edits

--- template/src/index.js
+++ template/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from "react-dom";
+import Presentation from "./presentation";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<Presentation/>, document.getElementById("root"));
 registerServiceWorker();
